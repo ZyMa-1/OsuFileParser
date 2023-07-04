96c84df95a5f2cd676eacaa84e142ef4f0bf5df7
ZyMa-2 <57665203+ZyMa-1@users.noreply.github.com>
2023-07-04 22:07:42 +0600

    setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,21 @@ from setuptools import setup, find_packages
 
 setup(
     name='OsuFileParser',
-    version='1.0',
+    version='1.0.0',
     description='Package for wrapping ".osu" file data into a python class.',
     author='ZyMa-1',
+    url='https://github.com/ZyMa-1/OsuFileParser',
+    license='MIT',
     packages=find_packages('src'),
     package_dir={'': 'src'},
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+    install_requires=[
+        # Specify any required dependencies here
+    ],
 )
